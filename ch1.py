5eb2cab9946bb850718c818c6fe91579257b41c8
Kyungjin <gulmok1217@hanyang.ac.kr>
2023-04-04 18:52:06 +0900

    세 정수를 입력받아 중앙값 구하기 1

--- ch1.py
+++ ch1.py
@@ -1,3 +1,24 @@
-print("이름을 입력하세요. : ", end="")
-name = input()
-print(f"안녕하세요? {name}님.")
+def med3(a, b, c):
+    if a > b:
+        if b >= c:
+            return b
+        elif c >= a:
+            return a
+        else:
+            return c
+
+    elif a > c:
+        return a
+
+    elif b > c:
+        return c
+
+    else:
+        return b
+
+
+print("세 정수를 입력하세요.")
+a = int(input("정수 a를 입력하세요 :"))
+b = int(input("정수 b를 입력하세요 :"))
+c = int(input("정수 c를 입력하세요 :"))
+print(f"중앙값은 {med3(a,b,c)}입니다.")
