8d2e1efbff768ade05f54cb7c61e3becb54594b7
Kyungjin <gulmok1217@hanyang.ac.kr>
2023-04-04 19:16:12 +0900

    a부터 b까지 정수의 합 구하기(for문)

--- ch1.py
+++ ch1.py
@@ -1,9 +1,13 @@
-print("1부터 n까지 정수의 합을 구합니다.")
-n = int(input("n값을 입력하세요.: "))
+print("a부터 b까지 정수의 합을 구합니다.")
+a = int(input("정수 a를 입력하세요.: "))
+b = int(input("정수 b를 입력하세요.: "))
+
+if a > b:
+    a, b = b, a
 
 sum = 0
 
-for i in range(1, n + 1):
+for i in range(a, b + 1):
     sum += i
 
-print(f"1부터 {n}까지의 정수의 합은 {sum}입니다.")
+print(f"{a}부터 {b}까지의 정수의 합은 {sum}입니다.")
