e02d3e2918e2567b80629c9f8a4ddcc13a4cb385
Kyungjin <gulmok1217@hanyang.ac.kr>
2023-04-04 18:58:08 +0900

    입력받은 정수의 부호(양수, 음수, 0) 출력하기

--- ch1.py
+++ ch1.py
@@ -1,14 +1,8 @@
-def med3(a, b, c):
-    if (b >= a and a >= c) or (c >= a and a >= b):
-        return a
-    elif (a >= b and b >= c) or (c > b and b >= a):
-        return b
-    else:
-        return c
+n = int(input("정수를 입력하세요.: "))
 
-
-print("세 정수를 입력하세요.")
-a = int(input("정수 a를 입력하세요 :"))
-b = int(input("정수 b를 입력하세요 :"))
-c = int(input("정수 c를 입력하세요 :"))
-print(f"중앙값은 {med3(a,b,c)}입니다.")
+if n > 0:
+    print("이 수는 양수입니다.")
+elif n < 0:
+    print("이 수는 음수입니다.")
+else:
+    print("이 수는 0입니다.")
