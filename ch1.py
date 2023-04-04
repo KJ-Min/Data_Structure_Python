dc6749656b46f695db8d77d04cbd915e997f8ca3
Kyungjin <gulmok1217@hanyang.ac.kr>
2023-04-04 19:00:08 +0900

    3개로 분기하는 조건문

--- ch1.py
+++ ch1.py
@@ -1,8 +1,8 @@
 n = int(input("정수를 입력하세요.: "))
 
-if n > 0:
-    print("이 수는 양수입니다.")
-elif n < 0:
-    print("이 수는 음수입니다.")
+if n == 1:
+    print("A")
+elif n == 2:
+    print("B")
 else:
-    print("이 수는 0입니다.")
+    print("C")
