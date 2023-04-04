068c6c664a09210a0a4f8a231b535db29300d9d2
Kyungjin <gulmok1217@hanyang.ac.kr>
2023-04-04 19:12:11 +0900

    1부터 n까지 정수의 합 구하기 2(for문)

--- ch1.py
+++ ch1.py
@@ -2,10 +2,8 @@ print("1부터 n까지 정수의 합을 구합니다.")
 n = int(input("n값을 입력하세요.: "))
 
 sum = 0
-i = 1
 
-while i <= n:
+for i in range(1, n + 1):
     sum += i
-    i += 1
 
 print(f"1부터 {n}까지의 정수의 합은 {sum}입니다.")
